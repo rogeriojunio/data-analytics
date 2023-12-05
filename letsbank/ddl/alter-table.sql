6897516f9d2940f8283df464f85e4e9c23f0af53
rogeriojunio <rogeriojunio.02@outlook.com>
2023-12-05 13:51:54 -0300

    new-database-test

---
+++ letsbank/ddl/alter-table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE cliente 
+ADD numero_cliente BIGINT
---
+++ letsbank/ddl/create-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cliente (
+	id_cliente BIGINT,
+	nome TEXT,
+	sexo CHAR,
+	endereco TEXT,
+	data_nascimento DATE,
+	ativo BOOLEAN
+)
